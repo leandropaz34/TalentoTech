ff72b20d10aa5a5a8c711c4c2a4be212eab54821
leandropaz34 <103154853+leandropaz34@users.noreply.github.com>
2024-12-28 17:50:37 -0300

    Update buttonBurger.js

--- js/buttonBurger.js
+++ js/buttonBurger.js
@@ -1,7 +1,14 @@
+// Seleccionar el botón y el menú
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav_MenuHorizontal');
+
+// Función para alternar la visibilidad del menú
 function toggleMenu() {
-    const menu = document.querySelector('.nav_MenuHorizontal');
     menu.classList.toggle('active');
 }
+
+// Escuchar el evento de clic en el botón hamburguesa
+hamburger.addEventListener('click', toggleMenu);
 
 // Hacer la función accesible globalmente
 window.toggleMenu = toggleMenu;
