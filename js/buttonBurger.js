917b294ffec7d0adbcd0810cf034a3b49ddf8caa
leandropaz34 <103154853+leandropaz34@users.noreply.github.com>
2024-12-28 17:40:11 -0300

    Update buttonBurger.js

--- js/buttonBurger.js
+++ js/buttonBurger.js
@@ -2,3 +2,6 @@ function toggleMenu() {
     const menu = document.querySelector('.nav_MenuHorizontal');
     menu.classList.toggle('active');
 }
+
+// Hacer la función accesible globalmente
+window.toggleMenu = toggleMenu;
